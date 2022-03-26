a59026aa99eb140a2fe7c96171763cf930eb768c
Houjun Liu <hliu@shabang.cf>
2022-03-25 23:35:17 -0700

    so many colours

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+function InfoBar() {
+    return (
+        <div className={styles.infobar}>
+          <span id="name">Houjun Liu</span>
+        </div>
+    );
+}
+
+function MainNav() {
+    return (
+        <div className={styles.navbar}>
+          <span className={styles.navbaritem} id={styles.projects}><Link href="/projects">Projects</Link></span>
+          <span className={styles.navbaritem} id={styles.research}><Link href="/research">Research</Link></span>
+          <span className={styles.navbaritem} id={styles.production}><Link href="/production">Production</Link></span>
+          <span className={styles.navbaritem} id={styles.about}><Link href="/about">About</Link></span>
+        </div>
+    );
+}
 
 export default function Home() {
     return (
-        <div>
-          a
+        <div className={styles.home}>
+          <MainNav />
+          <InfoBar />
         </div>
-    )
+    );
 }
