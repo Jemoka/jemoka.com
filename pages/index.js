2ab28a2b02a7ff26e8e2c973b24723e01fe28a62
Houjun Liu <hliu@shabang.cf>
2022-03-26 09:28:42 -0700

    responsive woo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Router from 'next/router';
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 import { useSpring, animated } from 'react-spring';
 
@@ -15,42 +15,42 @@ function InfoBar() {
 }
 
 function MainNav() {
-    const [ps, psAPI] = useSpring(() => ({ width: "25vw" }));
-    const [rs, rsAPI] = useSpring(() => ({ width: "25vw" }));
-    const [prods, prodsAPI] = useSpring(() => ({ width: "25vw" }));
-    const [as, asAPI] = useSpring(() => ({ width: "25vw" }));
+    const [ps, psAPI] = useSpring(() => ({ width: "25vw", height: "25vh" }));
+    const [rs, rsAPI] = useSpring(() => ({ width: "25vw", height: "25vh" }));
+    const [prods, prodsAPI] = useSpring(() => ({ width: "25vw", height: "25vh" }));
+    const [as, asAPI] = useSpring(() => ({ width: "25vw", height: "25vh" }));
 
     console.log(ps);
 
     return (
         <div className={styles.navbar}>
           <animated.span className={styles.navbaritem} id={styles.projects} style={ps}
-                         onMouseEnter={() => psAPI.start({width: "40vw"})}
-                         onMouseLeave={() => psAPI.start({width: "25vw"})}
+                         onMouseEnter={() => psAPI.start({width: "40vw", height: "40vh"})}
+                         onMouseLeave={() => psAPI.start({width: "25vw", height: "25vh"})}
                          onClick={()=> {
                              psAPI.start({width: "400vw"});
                              Router.push("/projects");
                          }}><a>Projects</a></animated.span>
 
           <animated.span className={styles.navbaritem} id={styles.research} style={rs}
-                         onMouseEnter={() => rsAPI.start({width: "40vw"})}
-                         onMouseLeave={() => rsAPI.start({width: "25vw"})}
+                         onMouseEnter={() => rsAPI.start({width: "40vw", height: "40vh"})}
+                         onMouseLeave={() => rsAPI.start({width: "25vw", height: "25vh"})}
                          onClick={()=> {
                              rsAPI.start({width: "400vw"});
                              Router.push("/research");
                          }}><a>Research</a></animated.span>
 
           <animated.span className={styles.navbaritem} id={styles.production} style={prods}
-                         onMouseEnter={() => prodsAPI.start({width: "40vw"})}
-                         onMouseLeave={() => prodsAPI.start({width: "25vw"})}
+                         onMouseEnter={() => prodsAPI.start({width: "40vw", height: "40vh"})}
+                         onMouseLeave={() => prodsAPI.start({width: "25vw", height: "25vh"})}
                          onClick={()=> {
                              prodsAPI.start({width: "400vw"});
                              Router.push("/production");
                          }}><a>Production</a></animated.span>
 
           <animated.span className={styles.navbaritem} id={styles.about} style={as}
-                         onMouseEnter={() => asAPI.start({width: "40vw"})}
-                         onMouseLeave={() => asAPI.start({width: "25vw"})}
+                         onMouseEnter={() => asAPI.start({width: "40vw", height: "40vh"})}
+                         onMouseLeave={() => asAPI.start({width: "25vw", height: "25vh"})}
                          onClick={()=> {
                              asAPI.start({width: "400vw"});
                              Router.push("/production");
