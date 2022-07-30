3a51b0665ee51c0ca7ed5b96e2b2394603548353
Houjun Liu <hliu@shabang.cf>
2022-07-30 15:43:41 -0700

    about.gif

--- pages/index.js
+++ pages/index.js
@@ -74,6 +74,7 @@ export default function Home() {
             <link rel="preload" href="./Research.gif" as="image"/>
             <link rel="preload" href="./Production.gif" as="image"/>
             <link rel="preload" href="./Projects.gif" as="image"/>
+            <link rel="preload" href="./About.gif" as="image"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
           </Head>
           <MainNav />
